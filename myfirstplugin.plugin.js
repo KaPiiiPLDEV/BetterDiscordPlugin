b64f136e3d5933c7d9ab4521c5d5df508844c2d1
KaPiiiPLDEV <113234227+KaPiiiPLDEV@users.noreply.github.com>
2022-09-10 10:31:30 +0200

    Add files via upload

---
+++ myfirstplugin.plugin.js
@@ -0,0 +1,23 @@
+/**
+ * @name first plugin
+ * @version 1.0.0
+ * @description My first plugin
+ * @author KaPii
+ * @updateUrl
+ */
+ 
+
+ module.exports = class hello{
+    
+    load() {}
+    start() {
+    
+
+    
+    }
+stop(){
+ class Stupidity{
+
+ }
+}
+}
