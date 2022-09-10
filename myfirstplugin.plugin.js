83db94345705c111182ae5d3595e9ac233a6f9ea
KaPiiiPLDEV <113234227+KaPiiiPLDEV@users.noreply.github.com>
2022-09-10 12:37:44 +0200

    update.pz

--- myfirstplugin.plugin.js
+++ myfirstplugin.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name first plugin
- * @version 1.1.5
+ * @version 1.2.0
  * @description My first plugin
  * @author KaPii
  * 
@@ -10,20 +10,17 @@
  
 
  module.exports = class hello{
+    unload() {BdApi.alert("bruh")};
     load() {};
     start() {
     const BdApi = window.BdApi;
     BdApi.alert("Hello! This is my first better discord plugin. its hard to make this");
-    blur.prototype;
+    BdApi.showToast("Thanks for using my first plugin! - From KaPiii");
     
-    if(this != this.source) {
-        BdApi.alert("Update availible", "https://github.com/KaPiiiPLDEV/BetterDiscordPlugin/blob/main/myfirstplugin.plugin.js")
-        
-    }
 
     
-    }
+    };
 stop(){
-BdApi.alert("wy u disable my stuff", "unless ur saving some change in my coed, there shuld be no need for dat!")
-}
-}
+BdApi.alert("wy u disable my stuff", "unless ur saving some change in my coed, there shuld be no need for dat!");
+};
+};
