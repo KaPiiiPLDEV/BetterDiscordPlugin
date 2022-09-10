21d5d9e0c7f996e89cce1194809f6e31323258df
KaPiiiPLDEV <113234227+KaPiiiPLDEV@users.noreply.github.com>
2022-09-10 10:45:05 +0200

    plug update

--- myfirstplugin.plugin.js
+++ myfirstplugin.plugin.js
@@ -1,9 +1,9 @@
 /**
  * @name first plugin
- * @version 1.0.0
+ * @version 1.1.0
  * @description My first plugin
  * @author KaPii
- * @updateUrl
+ * @updateUrl https://github.com/KaPiiiPLDEV/BetterDiscordPlugin/blob/main/myfirstplugin.plugin.js
  */
  
 
@@ -11,13 +11,13 @@
     
     load() {}
     start() {
-    
+    const BdApi = window.BdApi;
+    BdApi.alert("Hello! This is my first better discord plugin. its hard to make this")
+    blur.prototype
 
     
     }
 stop(){
- class Stupidity{
-
- }
+BdApi.alert("wy u disable my stuff")
 }
 }
