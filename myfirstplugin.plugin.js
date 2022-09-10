ebd8da59d7b6f2a47862465c1c6128667577b059
KaPiiiPLDEV <113234227+KaPiiiPLDEV@users.noreply.github.com>
2022-09-10 11:01:21 +0200

    plugin update

--- myfirstplugin.plugin.js
+++ myfirstplugin.plugin.js
@@ -1,23 +1,28 @@
 /**
  * @name first plugin
- * @version 1.1.0
+ * @version 1.1.2
  * @description My first plugin
  * @author KaPii
+ * 
+ * @source https://raw.githubusercontent.com/KaPiiiPLDEV/BetterDiscordPlugin/main/myfirstplugin.plugin.js
  * @updateUrl https://raw.githubusercontent.com/KaPiiiPLDEV/BetterDiscordPlugin/main/myfirstplugin.plugin.js
  */
  
 
  module.exports = class hello{
-    
-    load() {}
+    load() {};
     start() {
     const BdApi = window.BdApi;
-    BdApi.alert("Hello! This is my first better discord plugin. its hard to make this")
-    blur.prototype
+    BdApi.alert("Hello! This is my first better discord plugin. its hard to make this");
+    blur.prototype;
+    
+    if(this != this.updateUrl) {
+        BdApi.alert("pls update ")
+    }
 
     
     }
 stop(){
-BdApi.alert("wy u disable my stuff")
+BdApi.alert("wy u disable my stuff", "unless ur saving some change in my coed, there shuld be no need for dat!")
 }
 }
